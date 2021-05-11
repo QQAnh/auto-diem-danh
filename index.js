271e1aae25c3f1808f9cc40fa6015f4e0992bbc5
QQAnh <qqanh@QQAnhs-Mac-mini.local>
2021-05-11 16:08:43 +0700

    auto diem danh version 2

--- index.js
+++ index.js
@@ -1,71 +1,64 @@
 const schedule = require('node-schedule');
-var axios = require('axios');
+const axios = require('axios');
 
-const rule = new schedule.RecurrenceRule();
-rule.hour = 8;
-rule.minute = 0;
-rule.tz = 'Asia/Ho_Chi_Minh';
+const randomChao = ['hello mn', 'chào cả nhà', 'xin chào mn']
+const randomBye = ['Bye bye cả nhà', 'bye mn', 'chào mn']
 
-schedule.scheduleJob(rule, function(){
-    console.log(' function cron hello ');
+/**
+ * Token của chatops lấy trong session (Tự túc lấy đi)
+ * @type {string}
+ */
+const token = '5tg9r3xbstnb8n95nc7yox8rie'
 
-    const randomChao = ['hello mn', 'chào cả nhà', 'xin chào mn']
-    const x  = Math.floor(Math.random() * 3);
-    var dataHello = JSON.stringify({
-        "channel_id": "khda933sbbbsi8ff3t386o5ie mongoe",
-        "message": randomChao[x],
-        "root_id": "",
-        "props": {}
-    });
-    var config = {
-        method: 'post',
-        url: 'https://mm.nal.vn/api/v4/posts',
-        headers: {
-            'Authorization': 'Bearer ',
-            'Content-Type': 'application/json',
-        },
-        data : dataHello
-    };
-    axios(config)
-        .then(function (response) {
-            console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-            console.log(error);
+/**
+ *
+ * @param hour (giờ)
+ * @param minute (phút)
+ * @param timezone
+ * @param arrayText (bộ câu chào)
+ * @param range (số lượng câu chào)
+ * @param channel_id
+ */
+function auto_diem_danh(hour, minute, timezone, arrayText, range, channel_id) {
+    const rule = new schedule.RecurrenceRule();
+    rule.hour = hour;
+    rule.minute = minute;
+    rule.tz = timezone;
+
+    schedule.scheduleJob(rule, function(){
+        const x  = Math.floor(Math.random() * range);
+        var dataHello = JSON.stringify({
+            "channel_id": channel_id,
+            "message": arrayText[x],
+            "root_id": "",
+            "props": {}
         });
-});
-
-
-const rules = new schedule.RecurrenceRule();
-rules.hour = 17;
-rules.minute = 30;
-rules.tz = 'Asia/Ho_Chi_Minh';
-
-schedule.scheduleJob(rules, function(){
-    console.log(' function cron bye bye ');
-
-    const randomChao = ['Bye bye cả nhà', 'bye mn', 'chào mn']
-    const x  = Math.floor(Math.random() * 3);
-    var dataGoodbye = JSON.stringify({
-        "channel_id": "7aytssqs7bfyijeganx8o31rra",
-        "message": randomChao[x],
-        "root_id": "",
-        "props": {}
+        var config = {
+            method: 'post',
+            url: 'https://mm.nal.vn/api/v4/posts',
+            headers: {
+                'Authorization': 'Bearer '+ token,
+                'Content-Type': 'application/json',
+            },
+            data : dataHello
+        };
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     });
-    var config = {
-        method: 'post',
-        url: 'https://mm.nal.vn/api/v4/posts',
-        headers: {
-            'Authorization': 'Bearer ',
-            'Content-Type': 'application/json',
-        },
-        data : dataGoodbye
-    };
-    axios(config)
-        .then(function (response) {
-            console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-});
+}
+
+
+/**
+ * Điểm danh bắt đầu làm việc
+ */
+auto_diem_danh(8,0, 'Asia/Ho_Chi_Minh', randomChao, randomChao.length, 'khda933sbbbsi8ff3t386o5ie');
+
+/**
+ * Điểm danh ra về
+ */
+auto_diem_danh(17,30, 'Asia/Ho_Chi_Minh', randomBye, randomBye.length,'7aytssqs7bfyijeganx8o31rra');
